01ce530664eee81f2a7607754bf74bed1dc137c8
kubik <kubik20083@mail.ru>
2023-01-18 09:47:58 +0300

    Задача 1.

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Калькулятор. Возведение в степень.");
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень, в которою хотите возвести число");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = a;
+
+for (int i = 1; i < b; i++)
+{
+result = result * a;
+}
+Console.WriteLine($"{a} в степени {b} равно: " + result);
