1760cd1bebbc4894b5e0d31b0204adf88714314a
kubik <kubik20083@mail.ru>
2023-01-18 20:18:40 +0300

    решение задачи 3, длинна массива произвольная

---
+++ task3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.Write("Введите желаемую длину массива, программа заполнит его: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[number];
+System.Console.Write($"array[{number}] = ");
+for (int i = 0; i < number; i++)
+{
+    array[i] = new Random().Next(1, 100);
+    Console.Write(array[i] + ", ");
+}
+
+
+
+
